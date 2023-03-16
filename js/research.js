6a90fc1a9d9dbf1729c58fefec4bd5376edc329e
Esteban ELIAS PUEYO <Esteban.Elias-Pueyo@etu.univ-grenoble-alpes.fr>
2023-03-16 14:07:07 +0100

    Changes by delannyo

--- js/research.js
+++ js/research.js
@@ -40,4 +40,47 @@ class Research {
         const data = await rep.json();
         return this.tableauResultats(data);
     }
+
+    async appelApiQWo() {
+        let queryWo = "";
+        if (this._input.indexOf("NOT")!==-1) {
+            queryWo = this._input + this.MotsAEnlever;
+        } else {
+            queryWo = this._input + " NOT " + this.MotsAEnlever;
+        }
+        let url = "https://newsdata.io/api/1/news?apikey=" + this.cle + "&q=" + queryWo + "&language=" + this.language;
+        const rep = await fetch(url);
+        const data = await rep.json();
+        return this.tableauResultats(data);
+    }
+
+    tableauResultats(data) {
+        const tabNews = [];
+        tabNews.push(data["totalResults"]);
+        for (let i = 1; i < data["results"].length; i++) {
+            let news = new News(data["results"][i].title, data["results"][i].description, data["results"][i].link, data["results"][i]["country"][0]);
+            tabNews.push(news);
+        }
+        this._resultatDerniereRecherche = tabNews;
+        return tabNews
+    }
+    /*
+    setState () {
+        try {
+            const resp = await fetch(https://newsdata.io/api/1/news?apikey=" + this.cle + ;
+        }
+    }
+    */
+
+
+
+    getResultatDerniereRecherche() {
+        return this._resultatDerniereRecherche;
+    }
+
+    supprimerFavoris() {
+
+    }
+
+
 }
